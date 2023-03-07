03ea43c7a5d606d39b3ae94afac8d7a7a06c3a2d
不愿见识你的妩媚 <1115622221@qq.com>
2023-03-07 20:53:37 +0800

    master第四次提交

--- src/main/java/org/example/leetcode/Main.java
+++ src/main/java/org/example/leetcode/Main.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dev提交");
+        System.out.println("master提交");
         System.out.println("第三次提交");
         System.out.println("第二次提交");
         oneToN n = new oneToN();
